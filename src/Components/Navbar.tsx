967536c228b876ccb545d329fc6c75e37617aeb0
Sabreena Daniyal <160657530+sabreenadaniyal@users.noreply.github.com>
2024-11-13 01:55:29 +0500

    Update Navbar.tsx

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -3,26 +3,26 @@ import Link from "next/link";
 
 export default function Navbar(){
   return(
-    <div className="bg-slate-700">
-      <header className="text-gray-400 body-font w-full h-20">
-        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-
-         <Image className="md:ml-3"
+    <div className="bg-slate-700 lg:w-full md:w-full ">
+      <header className="text-gray-400 body-font lg:w-full md:flex flex-col md:w-full lg:h-16">
+        
+        <div className="container mx-full flex flex-wrap p-1 flex-col md:flex-row items-center">
+         <Image className="md:ml-5"
          src="/images/S.gif" alt="logo"
-         width={50}
+         width={55}
          height={50} />
          
-         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-           <Link href="/" className="mr-5 text-xl font-bold hover:text-blue-900">HOME</Link>
-           <Link href="/about" className="mr-5 text-xl font-bold hover:text-blue-900">ABOUT us</Link>
-           <Link href="/gallery" className="mr-5 text-xl font-bold hover:text-blue-900">GALLERY</Link>
-           <Link href="/career" className="mr-5 text-xl font-bold hover:text-blue-900">CAREER</Link>
-           <Link href="/projects" className="mr-5 text-xl font-bold hover:text-blue-900">PROJECTS</Link>
-           <Link href="/contact" className="mr-5 text-xl font-bold hover:text-blue-900">CONTACT</Link>
+         <nav className="lg:ml-[30%] flex flex-wrap items-center md:flex justify-center ">
+           <Link href="/" className="mr-7 text-lg md:text-sm font-bold hover:text-blue-900">HOME</Link>
+           <Link href="/about" className="mr-7 text-lg md:text-sm font-bold hover:text-blue-900">ABOUT us</Link>
+           <Link href="/gallery" className="mr-7 text-lg md:text-sm font-bold hover:text-blue-900">GALLERY</Link>
+           <Link href="/career" className="mr-7 text-lg md:text-sm font-bold hover:text-blue-900">CAREER</Link>
+           <Link href="/projects" className="mr-7 text-lg md:text-sm font-bold hover:text-blue-900">PROJECTS</Link>
+           <Link href="/contact" className="mr-7 text-lg md:text-sm font-bold hover:text-blue-900">CONTACT</Link>
          </nav>
 
           <Link href={"/images/cv.png"}>
-           <button className="inline-flex items-center bg-gray-800 border-0 py-2 px-5 focus:outline-none hover:bg-red-700 rounded text-base mt-4 md:mt-0">
+           <button className="items-center bg-gray-800 border-0 py-2 focus:outline-none hover:bg-red-700 rounded text-base mt-4 md:mt-0">
               Download CV
            </button></Link>
         </div>
