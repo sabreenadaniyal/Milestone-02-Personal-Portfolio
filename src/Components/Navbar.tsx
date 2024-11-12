a51d71e76e8990b6172516640cea2ec44ee7ed08
Sabreena Daniyal <160657530+sabreenadaniyal@users.noreply.github.com>
2024-11-12 07:17:37 +0500

    Update Navbar.tsx

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar(){
     return(
         <div>
         <header className="text-gray-400 body-font">
-  <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+  <div className="container mx-auto flex p-5 flex-col md:flex-row items-center">
 
       <Image className="md:ml-3"src="/images/S.gif" alt="logo"
       width={90}
