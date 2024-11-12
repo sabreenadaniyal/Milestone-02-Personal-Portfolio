128d247c649ce7f835484227bf2d8049590e29bd
Sabreena Daniyal <160657530+sabreenadaniyal@users.noreply.github.com>
2024-11-13 02:04:00 +0500

    Update Hero.tsx

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -20,7 +20,7 @@ export default function Hero() {
   return (
     <div>
       <section className="text-gray-600 body-font">
-        <div className="container mx-full flex px-5 py-24 md:flex-row flex-col items-center">
+        <div className="container mx-full lg:flex px-5 py-24 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-slate-300 md:ml-14 text-center">I am </h1>
 
