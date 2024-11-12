12a9c09c52a9f801a2e020cac2949766a40372c5
Sabreena Daniyal <160657530+sabreenadaniyal@users.noreply.github.com>
2024-11-12 07:28:10 +0500

    Update Navbar.tsx

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -3,21 +3,20 @@ import Link from "next/link";
 
 export default function Navbar(){
     return(
-        <div>
         <header className="text-gray-400 body-font">
-  <div className="container mx-auto flex p-5 flex-col md:flex-row items-center">
+  <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
 
-      <Image className="md:ml-3"src="/images/S.gif" alt="logo"
+      <Image className="ml-3"src="/images/S.gif" alt="logo"
       width={90}
       height={50} />
           
     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-      <Link href="/" className="mr-5 text-xl font-bold hover:text-blue-900">HOME</Link>
-      <Link href="/about" className="mr-5 text-xl font-bold hover:text-blue-900">ABOUT us</Link>
-      <Link href="/gallery" className="mr-5 text-xl font-bold hover:text-blue-900">GALLERY</Link>
-      <Link href="/career" className="mr-5 text-xl font-bold hover:text-blue-900">CAREER</Link>
-      <Link href="/projects" className="mr-5 text-xl font-bold hover:text-blue-900">PROJECTS</Link>
-      <Link href="/contact" className="mr-5 text-xl font-bold hover:text-blue-900">CONTACT</Link>
+      <Link href="/" className="mr-5 font-bold hover:text-blue-900">HOME</Link>
+      <Link href="/about" className="mr-5 font-bold hover:text-blue-900">ABOUT us</Link>
+      <Link href="/gallery" className="mr-5 font-bold hover:text-blue-900">GALLERY</Link>
+      <Link href="/career" className="mr-5 font-bold hover:text-blue-900">CAREER</Link>
+      <Link href="/projects" className="mr-5 font-bold hover:text-blue-900">PROJECTS</Link>
+      <Link href="/contact" className="mr-5  font-bold hover:text-blue-900">CONTACT</Link>
     </nav>
 
     <Link href={"/images/cv.png"}>
@@ -26,6 +25,5 @@ export default function Navbar(){
     </button></Link>
   </div>
 </header> 
-</div>
     )
 }
