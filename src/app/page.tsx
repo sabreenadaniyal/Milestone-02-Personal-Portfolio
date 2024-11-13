3e329511424f1a81fe8d929547d8c8b7ff03148a
Sabreena Daniyal <160657530+sabreenadaniyal@users.noreply.github.com>
2024-11-14 00:40:53 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,9 @@ import Contact from "./contact/page";
 
 export default function Home() {
   return (
-    <div>
+    <div><br /><br />
         
-        <Hero />
+        <Hero /><br /><br /><br /><br /><br /><br />
         <About />
         <Gallery />
         <Career />
