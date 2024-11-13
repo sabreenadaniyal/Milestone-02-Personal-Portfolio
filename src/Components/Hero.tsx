9a941ee15aae284e887733cbfd8929cf3017c11a
Sabreena Daniyal <160657530+sabreenadaniyal@users.noreply.github.com>
2024-11-14 00:39:22 +0500

    Update Hero.tsx

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -16,29 +16,36 @@ export default function Hero() {
     });
     AOS.refresh();
   }, []);
-
   return (
     <div>
-      <section className="text-gray-600 body-font">
-        <div className="container mx-full lg:flex lg:flex-row px-5 py-24 md:flex-row flex-col items-center">
-          <div className="lg:flex-grow md:w-1/2 lg:pr-24 lg:flex lg:flex-row md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-slate-300 md:ml-14 text-center">I am </h1>
+      <section className="text-gray-600 body-font mx-16 rounded-xl">
+        <div className="container mx-auto flex px-15 py-10 md:flex-row flex-col items-center">
+
+          <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center
+        text-center">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 -mt-10 font-medium text-gray-400">I am</h1>
 
             <div data-aos="zoom-in-right">
-            <h1 className="hidden text-4xl font-bold font-serif text-purple-950">SABREENA DANIYAL!</h1>
+            <h1 className="text-3xl text-blue-700 font-bold">SABREENA DANIYAL!</h1>
             </div>
 
-            <p className="mb-0 leading-relaxed text-slate-800 text-3xl ml-[8%]">Fashion Designer..</p>
-            <p className="mb-8 leading-relaxed text-slate-300 text-sm ml-[8%]">de-designer Couture Villas..</p>
-
+            <p className="mb-0 mr-14 leading-relaxed text-purple-800">
+            FASHION DESIGNER...
+            </p>
+            <p className="mb-8 mr-14 leading-relaxed text-gray-400">
+            de-designer Couture Villas.
+            </p>
           </div>
-          <div className="lg:max-w-lg md:w-1/2 w-5/6">
-            <Image className="rounded-3xl object-contain object-center" src="/images/IMG-2024.jpg" alt="hero"
-              width={200}
-              height={100}
+
+          <div className=" lg:max-w-lg lg:w-full md:w-1/2w-5/6 mb-10 md:mb-0">
+            <Image className="rounded-2xl mx-auto" src="/images/IMG-2024.jpg" alt="profile"
+            width={280}
+            height={40}
             />
           </div>
+
         </div>
+
       </section> <br />
     </div>
   )
